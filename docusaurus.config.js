667e5bc9f50ae4b57aacc1fd4bee7f93a36cf00e
kbuddhism <94115514+kbuddhism@users.noreply.github.com>
2023-07-02 22:40:49 +0200

    Update Copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -123,7 +123,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Scribo-Potato, Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
