195698009bb66d9f1621991fdc7f05a7a9e48a76
kbuddhism <94115514+kbuddhism@users.noreply.github.com>
2023-07-03 22:04:24 +0200

    simplify site

--- blog/2023-07-02-het-schoppende-kind/index.md
+++ blog/2023-07-02-het-schoppende-kind/index.md
@@ -9,4 +9,4 @@ Rond 12:50-13:00 waren er kinderen van ongeveer 4 tot 7 jaar die een beetje raar
 
 Ik schok me een hoedje! Snel deed ik de deur open en zag een kind van zes. Ik dacht iets van gek! Maar ging toch gewoon mijn handen wassen na het lekker schijten op de wc. Later zat ik gewoon lekker te genieten van een heerlijk stuk pizza. Maar toen ik weer wou shoppen zag ik dat kind weer met een heerlijke raketijsje lekker zul je denken tot dat de moeder van het kind het ijsje afpakte en het in de prullenbak wou gooien. Ik dacht echt iets van gemeno! Al mijn haat was weg. Ik vond het zielig maar ook terecht.
 
-![Het Schappende Kind - Copyright 2023 Scribo-Potato](./het-schoppende-kind.jpg)
+![Het Schappende Kind - Copyright 2023 Scribo Potato](./het-schoppende-kind.jpg)
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Scribo-Potato',
+  title: 'Scribo Potato',
   tagline: 'Het wekelijkse journaal 😁 📰',
   favicon: 'img/scribo-potato.png',
 
@@ -37,14 +37,9 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        docs: false,
         blog: {
+          routeBasePath: '/',
           showReadingTime: true
         },
         theme: {
@@ -60,70 +55,20 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'Scribo-Potato',
+        title: 'Scribo Potato',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Scribo Potato Logo',
           src: 'img/scribo-potato.png',
         },
         items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+          
         ],
       },
       footer: {
         style: 'dark',
         links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Scribo-Potato, Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Scribo Potato, Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
